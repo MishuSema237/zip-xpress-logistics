7c7c9fc8a1c8f20a6cfd2fd912e96c67c9de7996
MishuSema237 <mishusema237@gmail.com>
2026-02-11 18:56:39 -0500

    metadata retoch

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -23,7 +23,7 @@ const Home: React.FC = () => {
         <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-24">
           <div className="text-center">
             <h1 className="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl">
-              Global Shipping &<br />
+              Zip Xpress Global Shipping &<br />
               <span className='text-zip-red-500'>Logistics Solutions</span>
             </h1>
             <p className="mt-3 max-w-md mx-auto text-base text-gray-100 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
